a6841a108f4cb44c38d706a2222bbe8fe0286c60
Helix-26 <lokeshkumar27112001@gmail.com>
2023-09-13 08:17:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let notesContainer = document.querySelector(".notes-container");
+let createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+let showNotes = () => {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes()
+
+let updateStorage = () => {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "Images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove()
+        updateStorage();
+    }
+    else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function () {
+                updateStorage();
+            }
+        })
+    }
+})
